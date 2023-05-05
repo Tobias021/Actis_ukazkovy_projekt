b6654ab2890e8948c35e3e06e5e0aa4e77ad81d0
Tobiáš Tláskal <tobias.tlaskal@ccd.cz>
2023-05-05 19:30:12 +0200

    Initializr import
    Importován vygenerovaný projekt initializru.
    
    # Konfigurace projektu
    
    **Balíky** - Maven
    **Spring boot verze** - 3.0.6
    **Java verze** - 17

---
+++ src/main/java/cz/actis/ukazkovy_projekt/UkazkovyProjektApplication.java
@@ -0,0 +1,13 @@
+package cz.actis.ukazkovy_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkazkovyProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkazkovyProjektApplication.class, args);
+	}
+
+}
